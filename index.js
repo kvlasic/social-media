0dd7b95213d53846ada604379bfad7418fe263d7
kvlasic <kristijan.vlasic1@gmail.com>
2022-08-25 20:47:59 +0200

    add post routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use(morgan("common"));
 // ROUTES
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 // Start the server on port 8800
 app.listen(8800, () => {
