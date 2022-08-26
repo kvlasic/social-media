db0578c2330cf8f435cd5ef0e2049fcfbb0adda2
kvlasic <kristijan.vlasic1@gmail.com>
2022-08-26 16:20:59 +0200

    add share

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,5 +1,12 @@
 import "./feed.css";
+import Share from "../share/Share";
 
 export default function Feed() {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWraper">
+        <Share />
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/components/share/Share.js
@@ -0,0 +1,5 @@
+import "./share.css";
+
+export default function Share() {
+  return <div>Share</div>;
+}
